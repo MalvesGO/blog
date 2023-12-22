7f396979aad18708a5332efc729a11b6fe6875d7
marcelo.alves <marcelo.alves@pt.clara.net>
2023-12-22 17:03:48 +0000

    commit

---
+++ components/nav/Navbar.tsx
@@ -0,0 +1,29 @@
+"use client";
+import Link from "next/link";
+import React from "react";
+import LoginForm from "./LoginForm";
+import Profile from "./Profile";
+import { useUser } from "@/lib/store/user";
+
+export default function Navbar() {
+
+  const user = useUser((state) => state.user);
+
+  return (
+    <nav className="flex items-center justify-between">
+      <div className="group">
+        <Link href="/" className="text-2xl font-bold">
+          DailyBlog
+        </Link>
+        <div className="h-1 w-0 group-hover:w-full transition-all bg-green-500"></div>
+      </div>
+      {
+        user ? (
+          <Profile />
+        ) : (
+          <LoginForm />
+        )
+      }
+    </nav>
+  );
+}
---
+++ components/nav/Profile.tsx
@@ -0,0 +1,16 @@
+import { useUser } from "@/lib/store/user";
+import Image from "next/image";
+import React from "react";
+
+export default function Profile() {
+  const user = useUser((state) => state.user);
+  return (
+    <Image
+      src={user?.user_metadata.avatar_url}
+      alt={user?.user_metadata.user_name}
+      width={50}
+      height={50}
+      className="rounded-full ring-2 ring-green-500"
+    />
+  );
+}
