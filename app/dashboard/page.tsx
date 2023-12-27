660855a355bb1c642b9d6eacc49f6c4c6e6ba5cb
marcelo.alves <marcelo.alves@pt.clara.net>
2023-12-27 19:54:44 +0000

    table component

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import { Plus } from "lucide-react";
+import Link from "next/link";
+import React from "react";
+import BlogTable from "./components/BlogTable";
 
 export default function Dashboard() {
   return (
-    <div>Dashboard</div>
-  )
+    <div className="space-y-5">
+      <div className="flex items-center justify-between">
+        <h1 className="text-2xl font-bold">Blogs</h1>
+        <Link href="/dashboard/blog/create">
+          <Button variant="outline">
+            Create Blog
+            <Plus size={16} className="ml-2" />
+          </Button>
+        </Link>
+      </div>
+      <BlogTable />
+    </div>
+  );
 }
